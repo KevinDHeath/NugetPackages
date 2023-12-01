5ee8bf5afeefadefad8400ea1d21b17c3af11f12
Kevin Heath <heathkd@hotmail.com>
2023-12-01 08:25:29 -0500

    Added test harness and doc generation.

---
+++ src/TestHarness/OverrideConsoleApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Application.Helper;
+
+namespace TestHarness;
+
+/// <inheritdoc/>
+public class OverrideConsoleApp : ConsoleApp
+{
+	/// <inheritdoc/>
+	public override void ShowProgramInfo()
+	{
+		base.ShowProgramInfo();
+		Console.WriteLine( "Specific information requested for derived class." );
+	}
+}
