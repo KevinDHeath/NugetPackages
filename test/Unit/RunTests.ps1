8f1dae32f2fe8296f8b6c7a8fd96f8f978c1f11d
Kevin Heath <heathkd@hotmail.com>
2024-02-27 18:25:25 -0500

    Enable Core.Tests

--- test/Unit/RunTests.ps1
+++ test/Unit/RunTests.ps1
@@ -12,11 +12,11 @@ Write-Host Results "$results" -ForegroundColor Yellow
 
   Set-Location "$PSScriptRoot/$testProject"
 
-  #if( "$PSScriptRoot" -eq "$output" ) { dotnet clean -c Testing | Out-Null }
+  if( "$PSScriptRoot" -eq "$output" ) { dotnet clean -c Testing | Out-Null }
   dotnet build -c Testing  --no-restore
   dotnet test -c Testing --collect:"XPlat Code Coverage" --no-build --results-directory "$results"
   Set-Location $PSScriptRoot
 }
 
-#DotNet_Test 'Core.Tests'
-DotNet_Test 'Helper.Tests'
+DotNet_Test 'Core.Tests'
+#DotNet_Test 'Helper.Tests'
