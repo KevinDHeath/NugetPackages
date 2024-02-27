498334ccc33c8dc512095b8a213003fc375b73a3
Kevin Heath <heathkd@hotmail.com>
2024-02-27 16:42:28 -0500

    Change .NET Build action to run tests

---
+++ test/Unit/RunTests.ps1
@@ -0,0 +1,22 @@
+param ( [string]$outputRoot )
+if( "" -eq $outputRoot ) { $output = "$PSScriptRoot" } else { $output = "$outputRoot" }
+
+function DotNet_Test {
+  param($testProject)
+
+Write-Host Project "$PSScriptRoot\$testProject" -ForegroundColor Yellow
+
+  $results = "$output\TestResults\" + $testProject.split('.')[0]
+
+Write-Host Results "$results" -ForegroundColor Yellow
+
+  #Set-Location "$PSScriptRoot\$testProject"
+
+  #if( "$PSScriptRoot" -eq "$output" ) { dotnet clean -c Testing | Out-Null }
+  #dotnet build -c Testing  --no-restore
+  #dotnet test -c Testing --collect:"XPlat Code Coverage" --no-build --results-directory "$results"
+  #Set-Location $PSScriptRoot
+}
+
+DotNet_Test 'Core.Tests'
+DotNet_Test 'Helper.Tests'
