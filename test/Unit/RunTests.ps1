46816b19f0da09cc5433de6670c12be740c0e4f0
Kevin Heath <heathkd@hotmail.com>
2024-02-27 17:38:13 -0500

    Enable dotnet test

--- test/Unit/RunTests.ps1
+++ test/Unit/RunTests.ps1
@@ -4,19 +4,19 @@ if( "" -eq $outputRoot ) { $output = "$PSScriptRoot" } else { $output = "$output
 function DotNet_Test {
   param($testProject)
 
-Write-Host Project "$PSScriptRoot\$testProject" -ForegroundColor Yellow
+Write-Host Project "$PSScriptRoot/$testProject" -ForegroundColor Yellow
 
-  $results = "$output\TestResults\" + $testProject.split('.')[0]
+  $results = "$output/TestResults/" + $testProject.split('.')[0]
 
 Write-Host Results "$results" -ForegroundColor Yellow
 
-  #Set-Location "$PSScriptRoot\$testProject"
+  Set-Location "$PSScriptRoot/$testProject"
 
   #if( "$PSScriptRoot" -eq "$output" ) { dotnet clean -c Testing | Out-Null }
-  #dotnet build -c Testing  --no-restore
-  #dotnet test -c Testing --collect:"XPlat Code Coverage" --no-build --results-directory "$results"
-  #Set-Location $PSScriptRoot
+  dotnet build -c Testing  --no-restore
+  dotnet test -c Testing --collect:"XPlat Code Coverage" --no-build --results-directory "$results"
+  Set-Location $PSScriptRoot
 }
 
-DotNet_Test 'Core.Tests'
+#DotNet_Test 'Core.Tests'
 DotNet_Test 'Helper.Tests'
