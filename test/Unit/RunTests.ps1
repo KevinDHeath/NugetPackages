8dcae9d928fda9705b733416c849c5280d70b033
Kevin Heath <heathkd@hotmail.com>
2024-02-28 08:31:24 -0500

    Run tests on Windows

--- test/Unit/RunTests.ps1
+++ test/Unit/RunTests.ps1
@@ -4,18 +4,25 @@ if( "" -eq $outputRoot ) { $output = "$PSScriptRoot" } else { $output = "$output
 function DotNet_Test {
   param($testProject)
 
-Write-Host Project "$PSScriptRoot/$testProject" -ForegroundColor Yellow
+  $results = "$output\TestResults\" + $testProject.split('.')[0].ToLower()
 
-  $results = "$output/TestResults/" + $testProject.split('.')[0]
+#Write-Host Project "$PSScriptRoot\$testProject" -ForegroundColor Yellow
+#Write-Host Results "$results" -ForegroundColor Yellow
 
-Write-Host Results "$results" -ForegroundColor Yellow
-
-  Set-Location "$PSScriptRoot/$testProject"
-
+  Set-Location "$PSScriptRoot\$testProject"
   if( "$PSScriptRoot" -eq "$output" ) { dotnet clean -c Testing | Out-Null }
   dotnet build -c Testing  --no-restore
   dotnet test -c Testing --collect:"XPlat Code Coverage" --no-build --results-directory "$results"
   Set-Location $PSScriptRoot
+
+  if( "$PSScriptRoot" -eq "$output" ) {
+    Write-Host Generating reports... -ForegroundColor Yellow
+    $reports = "$results\*\coverage.cobertura.xml"
+    reportgenerator -reports:"$reports" -targetdir:"$results\reports\html" -verbosity:Warning
+    reportgenerator -reports:"$reports" -targetdir:"$results\reports" -reporttypes:MarkdownSummaryGithub -verbosity:Warning
+    reportgenerator -reports:"$reports" -targetdir:"$results\reports\badges" -reporttypes:Badges -verbosity:Warning
+    Remove-Item "$results\reports\badges\badge_shieldsio_*.svg" | Out-Null
+  }
 }
 
 DotNet_Test 'Core.Tests'
